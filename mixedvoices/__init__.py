b23903e900ee3bee7a3f2d4599eab70f52648f82
AbhinavTuli <abhinav.tuli99@gmail.com>
2025-01-02 22:50:25 +0530

    check keys on package import

--- mixedvoices/__init__.py
+++ mixedvoices/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from mixedvoices import constants, metrics
+from mixedvoices import constants, metrics, models
 from mixedvoices.core.project import create_project, load_project
 from mixedvoices.evaluation.agents.base_agent import BaseAgent
 from mixedvoices.evaluation.eval_prompt_generator import EvalPromptGenerator
@@ -8,4 +8,16 @@ from mixedvoices.evaluation.eval_prompt_generator import EvalPromptGenerator
 os.makedirs(constants.PROJECTS_FOLDER, exist_ok=True)
 os.makedirs(constants.TASKS_FOLDER, exist_ok=True)
 
+
+def check_keys():
+    required_keys = ["OPENAI_API_KEY"]
+    if models.TRANSCRIPTION_MODEL == "deepgram/nova-2":
+        required_keys.append("DEEPGRAM_API_KEY")
+
+    for key in required_keys:
+        if key not in os.environ:
+            raise ValueError(f"Environment variable {key} not set")
+
+
+check_keys()
 OPEN_AI_CLIENT = None
