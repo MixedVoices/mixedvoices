65dc4a66c970d59adcd1de2e74a11f10b1c500b2
AbhinavTuli <abhinav.tuli99@gmail.com>
2025-01-08 09:34:06 +0530

    fix logo path

--- mixedvoices/dashboard/config.py
+++ mixedvoices/dashboard/config.py
@@ -1,3 +1,4 @@
+import os
 from typing import Final
 
 from PIL import Image
@@ -5,7 +6,9 @@ from PIL import Image
 API_PORT: Final = 7760
 DASHBOARD_PORT: Final = 7761
 API_BASE_URL: Final = f"http://localhost:{API_PORT}/api"
-page_icon = Image.open("mixedvoices/dashboard/content/logo.png")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+logo_path = os.path.join(current_dir, "content", "logo.png")
+page_icon = Image.open(logo_path)
 DEFAULT_PAGE_CONFIG = {
     "page_title": "MixedVoices Dashboard",
     "page_icon": page_icon,
